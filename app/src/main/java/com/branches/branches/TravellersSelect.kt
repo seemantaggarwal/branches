49e811f6a261318f9c2e8a77c8aec8377e859a23
Seemant <piyush@Piyushs-MacBook-Air.local>
2018-10-26 21:13:12 +0530

    update

--- app/src/main/java/com/branches/branches/TravellersSelect.kt
+++ app/src/main/java/com/branches/branches/TravellersSelect.kt
@@ -1,12 +1,36 @@
 package com.branches.branches
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class TravellersSelect : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_travellers_select)
+        val spinner: Spinner = findViewById(R.id.Spinner)
+        ArrayAdapter.createFromResource(
+                this,
+                R.array.number_traveller,
+                android.R.layout.simple_spinner_dropdown_item
+        ).also { adapter ->
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            spinner.adapter = adapter
+        }
+        class SpinnerActivity : Activity(), AdapterView.OnItemSelectedListener {
+
+            override fun onItemSelected(parent: AdapterView<*>, view: View, pos: Int, id: Long) {
+
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>) {
+                spinner.setPrompt("Number of travellers")
+            }
+        }
     }
 }
